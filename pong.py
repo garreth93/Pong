d9b61289889ac49042857571bf61a2208cc8074b
garreth93 <nerod18@gmail.com>
2022-06-10 02:04:21 +0200

    Mejorado el campo, añadida red

--- pong.py
+++ pong.py
@@ -18,17 +18,20 @@ class Paleta(pygame.Rect):
 
 class Pong:
 
-    _ALTO = 640
-    _ANCHO = 480
-    _MARGEN_LATERAL = 40
+    _ANCHO = 800
+    _ALTO = 600
+    _MARGEN_LATERAL = 10
 
     _ANCHO_PALETA = 5
     _ALTO_PALETA = _ALTO / 5
 
+    red = pygame.Rect(_ANCHO/2-1, 0, 3, _ALTO)
+
     def __init__(self):
         print('Construyendo un objeto pong')
         pygame.init()
-        self.pantalla = pygame.display.set_mode((self._ALTO, self._ANCHO))
+
+        self.pantalla = pygame.display.set_mode((self._ANCHO, self._ALTO))
 
         self.jugador1 = Paleta(
             self._MARGEN_LATERAL,               # coordenada x (left)
@@ -37,17 +40,19 @@ class Pong:
             self._ALTO_PALETA)                  # alto (height)
 
         self.jugador2 = Paleta(
-            self._ANCHO-self._MARGEN_LATERAL-self._ANCHO_PALETA,
+            self._ANCHO - self._ANCHO_PALETA - self._MARGEN_LATERAL,
             (self._ALTO-self._ALTO_PALETA)/2,
             self._ANCHO_PALETA,
             self._ALTO_PALETA)
 
-   
+
+
     def bucle_principal(self):
         print('Estoy en el bucle principal')        
         while True:
             pygame.draw.rect(self.pantalla, (255, 255, 255), self.jugador1)
             pygame.draw.rect(self.pantalla, (255, 255, 255), self.jugador2)
+            pygame.draw.rect(self.pantalla, (255, 255, 255), self.red)
             pygame.display.flip()
 
 if __name__ == "__main__":
