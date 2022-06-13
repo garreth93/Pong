111a6cb29bffb041aba652bb585527f9e4bf039b
Sam <nerod18@gmail.com>
2022-06-13 23:50:25 +0200

    Añadido movimiento a paletas + pelota

--- pong.py
+++ pong.py
@@ -1,4 +1,20 @@
 import pygame
+from random import randint
+
+# Especificaciones de la paleta
+
+WIDTH_PADDLE = 15
+HEIGTH_PADDLE = 100
+
+# Medidas del campo
+
+WIDTH = 700
+HEIGHT = 500
+MARGEN_LATERAL = 10
+
+# Especificaciones de la Pelota
+
+BALL_SIZE = 15
 
 '''
     - algo de herencia
@@ -12,20 +28,40 @@ import pygame
     - metodo para interactuar con la pelota??
 '''
 
-class Paleta(pygame.Rect):
+class Paddle(pygame.Rect): 
     _COLOR = (255, 255, 255)
-    pass
 
+    UP = True
+    DOWN = False
 
+    def __init__(self, x, y):
+        super(Paddle, self).__init__(x , y, WIDTH_PADDLE, HEIGTH_PADDLE)
+        self.speed = 5
+
+    def move_paddle(self, direction):
+        if direction == self.UP:
+            self.y = self.y - self.speed
+            if self.y < 0:
+                self.y = 0
+        else:
+            self.y = self.y + self.speed
+            if self.y > HEIGHT - HEIGTH_PADDLE:
+                self.y = HEIGHT - HEIGTH_PADDLE
+    
+class Ball(pygame.Rect):
+    BALL_COLOR = (0, 0, 255)
+
+    def __init__(self):
+        super(Ball, self).__init__((WIDTH -BALL_SIZE)/2, (HEIGHT-BALL_SIZE)/2,
+                BALL_SIZE, BALL_SIZE)
+        self.speed_x = randint(-5, 5)
+        self.speed_y = randint(-5, 5)
+
+    def ball_move(self):
+        self.y = self.y + self.speed_y
+        self.x = self.x + self.speed_x
+ 
 class Pong:
-    # Medidas del campo
-    _ANCHO = 700
-    _ALTO = 500
-    _MARGEN_LATERAL = 10
-
-    # Especificaciones de la paleta
-    _ANCHO_PALETA = 15
-    _ALTO_PALETA = _ALTO / 5
 
     # Especificaciones de la red        
     _NET_COLOR = (255, 0, 0)    
@@ -36,35 +72,39 @@ class Pong:
     def __init__(self):        
         pygame.init()
 
-        self.win = pygame.display.set_mode((self._ANCHO, self._ALTO), 0, 0)
+        self.win = pygame.display.set_mode((WIDTH, HEIGHT), 0, 0)
+        self.clock = pygame.time.Clock()
         pygame.display.set_caption('Pong')
 
-        self.jugador1 = Paleta(
-            self._MARGEN_LATERAL,               # coordenada x (left)
-            (self._ALTO-self._ALTO_PALETA)/2,   # coordenada y (top)
-            self._ANCHO_PALETA,                 # ancho (width)
-            self._ALTO_PALETA)                  # alto (height)
+        self.player1 = Paddle(
+            MARGEN_LATERAL,               # coordenada x (left)
+            (HEIGHT - HEIGTH_PADDLE)/2)       # coordenada y (top)
+                             
 
-        self.jugador2 = Paleta(
-            self._ANCHO - self._ANCHO_PALETA - self._MARGEN_LATERAL,
-            (self._ALTO-self._ALTO_PALETA)/2,
-            self._ANCHO_PALETA,
-            self._ALTO_PALETA)
+        self.player2 = Paddle(
+            WIDTH - WIDTH_PADDLE - MARGEN_LATERAL,
+            (HEIGHT - HEIGTH_PADDLE )/2)
+
+        self.ball = Ball()
+        
 
     def draw(self):
 
         self.win.fill(self._BACKGROUND_COLOR)
-        pygame.draw.rect(self.win, Paleta._COLOR , self.jugador1, 0, 10)
-        pygame.draw.rect(self.win, Paleta._COLOR, self.jugador2, 0, 10)
-        for i in range(10, self._ALTO, self._ALTO//10):
+        pygame.draw.rect(self.win, Paddle._COLOR , self.player1, 0, 10)
+        pygame.draw.rect(self.win, Paddle._COLOR, self.player2, 0, 10)
+        for i in range(10, HEIGHT, HEIGHT//10):
             if i % 2 == 1:
                 continue
-            pygame.draw.rect(self.win, self._NET_COLOR, (self._ANCHO//2 - 5, i, 5, self._ALTO//20), 0, 10)
-
+            pygame.draw.rect(self.win, self._NET_COLOR, (WIDTH//2 - 5, i, 5, HEIGHT//20), 0, 10)
+        
+        pygame.draw.rect(self.win, Ball.BALL_COLOR, self.ball, 0, 10)
 
         pygame.display.update()        
+        self.clock.tick(60)
+    # BUCLE PRINCIPAL DEL JUEGO
 
-    def bucle_principal(self):        
+    def main_cicle(self):        
         exit_game = False        
         while not exit_game:            
             for i in pygame.event.get():
@@ -73,11 +113,23 @@ class Pong:
                 if i.type == pygame.KEYDOWN:
                     if i.key == pygame.K_ESCAPE:
                         exit_game = True
+                    
+            key_state = pygame.key.get_pressed()
+            if key_state[pygame.K_a]:
+                self.player1.move_paddle(Paddle.UP)
+            if key_state[pygame.K_z]:
+                self.player1.move_paddle(Paddle.DOWN)
+            if key_state[pygame.K_UP]:
+                self.player2.move_paddle(Paddle.UP)
+            if key_state[pygame.K_DOWN]:
+                self.player2.move_paddle(Paddle.DOWN)
             
+            self.ball.ball_move()
+ 
             self.draw()           
 
 if __name__ == "__main__":
-    juego = Pong()
-    juego.bucle_principal() 
+    game = Pong()
+    game.main_cicle() 
 
         
